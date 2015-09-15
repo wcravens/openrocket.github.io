be50f6ef507f23213dfcb31708598a16f42307c1
Wes Cravens <wcravens@cortex-it.com>
2015-09-15 18:17:37 -0500

    Use bower to install bootstrap and bootswatch-paper-wiredep
     - Remove original jquery and let Bower manage it
     - User grunt-wiredep to automatically add Bower components
       to css and script references in index.html.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,21 @@ module.exports = function( grunt ) {
       livereload: {
         files: [ '*.html', '*.css', '*.js', '*.{png,jpg,jpeg,gif,webp,svg}' ],
         options: { livereload: true }
+      },
+      bower: {
+        files: ['bower_components/*', 'bower.json'],
+        tasks: ['wiredep']
+      }
+    },
+    wiredep: {
+      task: {
+        src: 'index.html'
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-wiredep');
 
   grunt.registerTask('default', ['watch'] );
 };
